89bc0c56839ac1e04b9464ae65518a065f303733
Vamsi Dath <vamsi@Vamsis-MacBook-Air.local>
2024-11-22 01:18:28 -0600

    test db connection

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const mysql = require('mysql2'); // Ensure this is 'mysql2'
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-// Set up the MySQL connection pool
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'pms-password',
     database: 'pms'
 });
 
-// Route to fetch parking spots
 app.get('/api/parking-spots', (req, res) => {
-    db.execute('SELECT * FROM ParkingSlot', [], (err, results) => {
+    db.query('SELECT * FROM ParkingSlot', (err, result) => {
         if (err) {
-            console.error('Error executing query:', err);
-            return res.status(500).json({ error: err });
+            console.log(err);
+            res.sendStatus(500);
+            return;
         }
-        res.json(results); // Send the results as JSON
+
+        res.json(result);
     });
 });
 
-// Start the Express server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
