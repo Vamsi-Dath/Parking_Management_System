b42e5a0f74f3369f807b52338aff81e751cdd3b9
Vamsi Dath <vamsi@Vamsis-MacBook-Air.local>
2024-11-22 01:23:01 -0600

    added other endpoints

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,77 @@ app.get('/api/parking-spots', (req, res) => {
     });
 });
 
+app.get('/api/parking-zones', (req, res) => {
+    db.query('SELECT * FROM ParkingZone', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+app.get('/api/owners', (req, res) => {
+    db.query('SELECT * FROM Owner', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
+app.get('/api/owner-phone-directory', (req, res) => {
+    db.query('SELECT * FROM OwnerPhoneDirectory', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
+app.get('/api/incharges', (req, res) => {
+    db.query('SELECT * FROM Incharge', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
+app.get('/api/incharge-phone-directory', (req, res) => {
+    db.query('SELECT * FROM InchargePhoneDirectory', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
+app.get('/api/vehicles', (req, res) => {
+    db.query('SELECT * FROM Vehicle', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(result);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
